808c1abac38ad110886fd753f81a56aba5a9e3d0
Brandon Sanchez <bsanchez26@live.com>
2022-04-25 20:18:00 -0700

    Added var futureCard

--- Script.js
+++ Script.js
@@ -85,4 +85,23 @@ function futureCondition(lat, lon)
                 temp: futureResponse.daily[i].temp.day,
                 humidity: futureResponse.daily[i].humidity
 
+                var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+                var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
                 
+                var futureCard = $(`
+                <div class="pl-3">
+                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
+                        <div class="card-body">
+                            <h5>${currDate}</h5>
+                            <p>${iconURL}</p>
+                            <p>Temp: ${cityInfo.temp} °F</p>
+                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                        </div>
+                    </div>
+                <div>
+            `);
+
+            $("#fiveDay").append(futureCard);
+        }
+    }); 
+}                
